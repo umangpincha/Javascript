a351b036f5bbb11e89a32f8e69d81711a645fedf
Umang Pincha <umangpincha@gmail.com>
2026-08-01 20:00:37 +0000

    Add 01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 1234;
+let accountEmail = "umang@gmail.com";
+let accountPassword = "38u483";
+accountCity = "Jaipur";
+
+// accountID = 3 (not allowed)
+accountEmail = "123@gmail.com"
+accountPassword = "3445"
+accountCity = "Banglore"
+let accountState;
+console.table([accountEmail , accountPassword , accountCity, accountState])
+
+// TODO:
+// Never use var , always prefer "const" if not then "let"
