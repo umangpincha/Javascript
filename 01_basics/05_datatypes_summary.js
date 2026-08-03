8d8ccd4967f8900d0b42a374046b6037c4019154
Umang Pincha <umangpincha@gmail.com>
2026-08-03 06:38:22 +0000

    datatype summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,15 @@
+//TODO:
+// DATATYPES
+//1) Primitive(call by value): number , string , boolean , null , undefined , Symbol , BigInt.
+
+// Non Primitive(or reference type)
+// Arrays , Objects, Functions
+// arrays -> []
+// objects => {name:"Umang" , age:18}
+
+let myFunc = function(){
+    console.log("my var");
+}
+myFunc();
+
+//typeof - type janane ke liye
