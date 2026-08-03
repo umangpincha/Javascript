f7fa63cb99bb2eefc47e6774302b84c3bc83aa16
Umang Pincha <umangpincha@gmail.com>
2026-08-03 08:21:57 +0000

    stack vs heap

--- 01_basics/05_datatypes_summary.js
+++ 01_basics/05_datatypes_summary.js
@@ -13,3 +13,29 @@ let myFunc = function(){
 myFunc();
 
 //typeof - type janane ke liye
+
+// TODO:
+
+// Stack (Primitive)
+// Heap (Non Primitive)
+
+// Stack
+let myYoutubeName = "explorewithumang"
+let anothername = myYoutubeName //copy di gayi hai, original value ka ref pass nhn kia gya
+anothername = "umang.explores"
+// console.log(myYoutubeName , anothername);
+
+// Heap
+let userOne = {
+    name:"Umang",
+    age: 18
+}
+let userTwo = userOne;// ref pass kia gaya , and aab userOne , usetTwo dono dono ek hi object ko point krte hain 
+userTwo.name = "Hitesh Sir"; 
+console.log(userOne.name); 
+
+
+
+
+
+
