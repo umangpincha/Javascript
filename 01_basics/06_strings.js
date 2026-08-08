bbcc692ad1f1c84a54a6263c00cf2e2cc9466933
Umang Pincha <umangpincha@gmail.com>
2026-08-08 06:54:08 +0000

    string , numbers

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,18 @@
+const name = "Umang";
+const age = 18;
+
+// console.log(`My name is ${name}`);
+let newGame = new String("Umang")
+// console.log(newGame[0]);
+// console.log(newGame.__proto__);
+let myslice = newGame.substring(0,4)
+// console.log(myslice);
+// console.log(newGame);
+
+let newString = "   Umang.   " 
+let myTrimString = newString.trim(); //remove whitespaces
+console.log(myTrimString);
+let url = "https://umang.com/umang%pincha"
+console.log(url.includes("sundar"));
+console.log(url.split("//"));
+
---
+++ 01_basics/07_num_and_maths.js
@@ -0,0 +1,16 @@
+const score = Number(400)
+const bal = new Number(400)
+// console.log(bal.toFixed(2));
+
+// Maths
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5)); 
+
+console.log(Math.floor(Math.random() * 6) + 1);
+let min = 10;
+let max = 20;
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+console.log(randomNum);
