82ace7c7529ae4f110ffa516f2e396c281f0ea1c
Umang Pincha <umangpincha@gmail.com>
2026-08-08 16:03:55 +0000

    08 date and time

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,14 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+// console.log(myDate.toString());
+
+// let myCreatedDate = new Date(2026 , 0 , 12);
+// let myCreatedDate = new Date("2026-08-01");
+// let myCreatedDate = new Date("01-08-2026");
+// console.log(myCreatedDate.toDateString());
+
+let newDate = new Date();
+console.log(newDate.getMonth());
+console.log(newDate.getDate());
